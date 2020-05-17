f6dcdff4050cc3af9f85ebe86c435a9f72ce5323
root <root@BRUNOM.localdomain>
2020-05-17 19:10:24 -0300

    refactoring var

--- Validar_RGB/js/script.js
+++ Validar_RGB/js/script.js
@@ -1,14 +1,14 @@
 window.addEventListener('load', readInput);
 
 function readInput() {
-  var val1 = document.querySelector("#range1").value;
-  var val2 = document.querySelector("#range2").value;
-  var val3 = document.querySelector("#range3").value;
+  var r = document.querySelector("#range1").value;
+  var g = document.querySelector("#range2").value;
+  var b = document.querySelector("#range3").value;
 
 
-document.querySelector("#value1").value = val1;
-document.querySelector("#value2").value = val2;
-document.querySelector("#value3").value = val3;
+document.querySelector("#value1").value = r;
+document.querySelector("#value2").value = g;
+document.querySelector("#value3").value = b;
 
-document.querySelector(".rgb-color").style.backgroundColor = "rgb(" + val1 + "," + val2 + "," + val3 +")";
+document.querySelector(".rgb-color").style.backgroundColor = "rgb(" + r + "," + g + "," + b +")";
 }
